87d43d355ecbdec4a2b8b20b424147b115d794b9
Root User <admin@demo.com>
2021-09-12 01:59:43 +0530

    Updates to Code Quality Checks

--- plugins/db_verify.php
+++ plugins/db_verify.php
@@ -17,21 +17,38 @@ if(_db()) {
     }
     
     $colMissing = array_diff([
-                "id","guid","created_by","created_on","edited_by","edited_on",
+                "id","guid",
+                "groupuid",//"privilegeid",
                 "blocked",
-                "groupuid","privilegeid",
+                "created_by","created_on","edited_by","edited_on",
               ],array_keys($cols));
     if(count($colMissing)>0) {
       $out2[] = "<div class='row'><div class='col-md-8 text-left'>{$tbl}</div><div class='col-md-4 hinttext'>".implode(", ",$colMissing)."</div></div>";
     }
+    
+    if(!isset($cols['id'])) {
+        $out2[] = "<div class='row'><div class='col-md-8 text-left'>{$tbl}</div><div class='col-md-4 hinttext'>ID missing</div></div>";
+    }
+    if(isset($cols['id'])) {
+        if($cols['id'][3]!="PRI") {
+            $out1[] = "<div class='row'><div class='col-md-8 text-left'>{$tbl}</div><div class='col-md-4 hinttext'>ID Is not Primary Column</div></div>";
+        }
+        if($cols['id'][5]!="auto_increment") {
+            $out1[] = "<div class='row'><div class='col-md-8 text-left'>{$tbl}</div><div class='col-md-4 hinttext'>ID Is not Auto Increament</div></div>";
+        }
+    }
 //     printArray([$cols,$keys,$keyP]);
   }
   
-  printResultBlock($out1,"Keys Verification",6);
-  printResultBlock($out2,"Missing Columns",6);
-//   printResultBlock($out3,"",6);
-//   printResultBlock($out4,"",6);
-//   printResultBlock($out5,"",6);
+  if((count($out1)+count($out2))<=0) {
+        echo "No issues found in the configured database";
+  } else {
+        printResultBlock($out1,"Keys Verification",6);
+        printResultBlock($out2,"Missing Columns",6);
+        // printResultBlock($out3,"",6);
+        // printResultBlock($out4,"",6);
+        // printResultBlock($out5,"",6);
+  }
 } else {
   echo "No database configured for the app";
 }
