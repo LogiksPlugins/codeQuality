d88cd17c6a320073e42ad147635fd743f1e7a76f
Root User <admin@demo.com>
2022-02-25 09:45:56 +0530

    Update to db verify plugin

--- plugins/db_verify.php
+++ plugins/db_verify.php
@@ -1,11 +1,16 @@
 <?php
 if(!defined('ROOT')) exit('No direct script access allowed');
 
+function test_txtcols($var) {
+  return (strtolower($var[1])=="text");// || strtolower($var[1])=="varchar(255)"
+}
+
+$out1 = [];$out2 = [];$out3 = [];$out4 = [];$out5 = [];
+
 if(_db()) {
   $dbInfo = _db()->get_dbObjects();
   $dbTables = _db()->get_tableList();
   
-  $out1 = [];$out2 = [];$out3 = [];$out4 = [];$out5 = [];
   foreach($dbTables as $tbl) {
     $cols = _db()->get_columnList($tbl,false);
     $keys = _db()->get_allkeys($tbl);
@@ -37,15 +42,22 @@ if(_db()) {
             $out1[] = "<div class='row'><div class='col-md-8 text-left'>{$tbl}</div><div class='col-md-4 hinttext'>ID Is not Auto Increament</div></div>";
         }
     }
-//     printArray([$cols,$keys,$keyP]);
+
+    $textCols = array_filter($cols,"test_txtcols");
+    if(count($textCols)>2) {
+      $out3[] = "<div class='row'><div class='col-md-8 text-left'>{$tbl}</div><div class='col-md-4 hinttext'>".
+        count($textCols)." Text Columns</div></div>";
+    }
+    
+    //printArray([$cols,$keys,$keyP]);exit();
   }
   
-  if((count($out1)+count($out2))<=0) {
+  if((count($out1)+count($out2)+count($out3)+count($out4)+count($out5))<=0) {
         echo "No issues found in the configured database";
   } else {
         printResultBlock($out1,"Keys Verification",6);
         printResultBlock($out2,"Missing Columns",6);
-        // printResultBlock($out3,"",6);
+        printResultBlock($out3,"Too Many Text Columns",6);
         // printResultBlock($out4,"",6);
         // printResultBlock($out5,"",6);
   }
